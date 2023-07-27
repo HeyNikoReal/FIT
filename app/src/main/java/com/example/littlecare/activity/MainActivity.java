e9d8029e72a4d9a56ddbde9f0f1b7ae3711f206e
Nicholas <heyniko911@gmail.com>
2023-07-27 13:39:45 +0700

    FIT Competition

---
+++ app/src/main/java/com/example/littlecare/activity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.littlecare.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.littlecare.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button login,signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.btn_login);
+        signUp = findViewById(R.id.btn_sign_up);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+    }
+}
